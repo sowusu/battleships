46299aea5948c66c4ee1d26cf78bc75fda3f9eca
Nana Kwame Owusu <nowusu@fandm.edu>
2015-03-27 09:29:02 -0500

    Initial Commit

---
+++ battle/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  battle
+//
+//  Created by Nana Kwame Owusu on 3/27/15.
+//  Copyright (c) 2015 Nana Kwame Owusu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
